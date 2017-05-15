1d747c2233d75e334beaf8f7aec02fdf7c5243f6
samturner3 <sam.turner@students.mq.edu.au>
2017-05-15 11:08:27 +1000

    AddedExternalDB

--- models/Ingredient.js
+++ models/Ingredient.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 // database is called recipes
-mongoose.connect('mongodb://localhost/recipes');
+// mongoose.connect('mongodb://localhost/recipes');
+mongoose.connect('mongodb://admin:admin@ds141401.mlab.com:41401/recipe');
 const { connection: db } = mongoose;
 
 db.on('error', console.error.bind(console, 'connection error:'));
